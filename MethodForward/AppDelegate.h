97eff1b91346bff45f0a9c5f1fd4d1a6fffc0d34
wangzhipan <wangzhipan@yy.com>
2018-09-26 16:26:31 +0800

    Initial Commit

---
+++ MethodForward/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MethodForward
+//
+//  Created by wzp on 2018/9/26.
+//  Copyright © 2018 wzp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MethodForward/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MethodForward
+//
+//  Created by wzp on 2018/9/26.
+//  Copyright © 2018 wzp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
